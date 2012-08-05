f3336413e1ca8ca469e77036effb83a685df7f48
Andreas Simon <mail@andreas-simon.info>
2012-08-05 08:53:58 +0200

    Basic Ruby version

---
+++ RubyProject/spec/foo_spec.rb
@@ -0,0 +1,15 @@
+require "rspec"
+require_relative "../lib/classes"
+
+describe "Foo" do
+
+  describe "#to_s" do
+
+    it "prints the description" do
+      # code goes here
+      false.should be_true
+    end
+
+  end
+
+end
