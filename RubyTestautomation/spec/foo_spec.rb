37993b68988f70fef74e9c48a9d813adf6222716
Andreas Simon <mail@andreas-simon.info>
2012-08-25 09:04:33 +0200

    Created template for Testautomation Code Retreat

---
+++ RubyTestautomation/spec/foo_spec.rb
@@ -0,0 +1,15 @@
+require "rspec"
+require_relative "../classes"
+
+describe "Foo" do
+
+  describe "#to_s" do
+
+    it "prints the description" do
+      # code goes here
+
+    end
+
+  end
+
+end
