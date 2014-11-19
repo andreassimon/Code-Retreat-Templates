7e529ef6bae68f0fd8f448d724c088461055787c
Andreas Simon <mail@andreas-simon.info>
2014-11-19 11:01:20 +0100

    Added Clojure setup

---
+++ clojure/src/gdcr/core.clj
@@ -0,0 +1,15 @@
+(ns gdcr.core)
+
+(def cardinal-points
+  (remove #{[0 0]} (for [a [-1 0 1], b [-1 0 1]] [a b])))
+
+(defn tick [world] world)
+
+(defn alive? [x y world]
+      (.contains world [x y]))
+
+(defn put-cell [x y world]
+      (list [x y]))
+
+(defn a-world []
+      '())
