98b8d1aec7c3aaf7805728dd9daee6fb87066c95
Andreas Simon <a.simon@quagilis.de>
2015-02-05 22:35:46 +0100

    Added Docker setup for PHP

--- php/tests/TheTest.php
+++ php/tests/TheTest.php
@@ -6,6 +6,7 @@ class TheTest extends PHPUnit_Framework_TestCase
     /** @test */
     public function test_the_truth()
     {
+        // https://phpunit.de/manual/current/en/appendixes.assertions.html
         $this->assertEquals(true, false);
     }
 
