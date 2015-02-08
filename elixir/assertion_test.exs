3d616453865d3596da53f703bb8d2b85b7829650
Andreas Simon <mail@andreas-simon.info>
2015-02-08 19:34:45 +0100

    Basic operators

--- elixir/assertion_test.exs
+++ elixir/assertion_test.exs
@@ -12,4 +12,31 @@ defmodule AssertionTest do
     # http://elixir-lang.org/docs/master/ex_unit/ExUnit.Assertions.html
     assert true
   end
+
+  test "Atoms" do
+    assert :hello == :hello
+  end
+
+  test "strings" do
+    assert "hello #{:world}" == "hello #{:world}"
+    assert "hello
+world" == "hello\nworld"
+  end
+
+  test "anonymous functions" do
+    add = fn a, b -> a+b end
+    assert is_function(add)
+    assert 3 == add.(1, 2)
+  end
+
+  test "lists" do
+    assert [1, 2] ++ [true] ++ [3] == [1, 2, true, 3]
+    assert 1 == hd([1, 2, 3])
+    assert [2, 3] == tl([1, 2, 3])
+    assert 'hello' == [104, 101, 108, 108, 111]
+  end
+
+  test "tuples" do
+    assert {0, "world"} == put_elem({0, :hello}, 1, "world")
+  end
 end
