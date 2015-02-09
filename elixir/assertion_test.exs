249eda8f8d5d9c0afaf35c70712aa3254a758045
Andreas Simon <mail@andreas-simon.info>
2015-02-09 21:54:03 +0100

    Case, cond, and if

--- elixir/assertion_test.exs
+++ elixir/assertion_test.exs
@@ -50,3 +50,61 @@ defmodule PatternMatchingTest do
     assert a == :hello
   end
 end
+
+defmodule CaseCondAndIfTest do
+  use ExUnit.Case, async: true
+
+  x = 1
+  test "case" do
+    case {1, 2, 3} do
+      {1, y, 3} when y > 0 -> "Will match"
+      _  -> "Won't match"
+    end
+  end
+
+  test "guard clauses in anonymous functions" do
+    f = fn
+      x, y when x > 0 -> x+y
+      x, y -> x*y
+    end
+    assert 4 == f.(1, 3)
+    assert -3 == f.(-1, 3)
+  end
+
+  test "cond" do
+    assert "But this will" == (cond do
+      2 + 2 == 5 -> "This will not be true"
+      2 * 2 == 3 -> "Nor this"
+      1 + 1 == 2 -> "But this will"
+      true       -> "This is always true (equivalent to else)"
+    end)
+  end
+
+  test "if / unless" do
+    assert "This works" == (if true do
+      "This works"
+    end)
+     
+    assert nil == (unless true do
+      "This will never be seen"
+    end)
+
+    assert "This will" == (if nil do
+      "This won't be seen"
+    else
+      "This will"
+    end)
+  end
+
+  test "do/end blocks" do
+    assert 4 == (if true, do: 1+3)
+
+    assert 13 == (if true, do: (
+      a = 1+2
+      a+10
+    ))
+
+    assert :that == (if false, do: :this, else: :that)
+  end
+
+end
