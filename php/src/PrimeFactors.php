5f806e641ef06861ce3b7d853028a971eb130510
Andreas Simon <mail@andreas-simon.info>
2015-12-15 17:55:45 +0100

    Implemented PrimeFactors functionality

--- php/src/PrimeFactors.php
+++ php/src/PrimeFactors.php
@@ -2,13 +2,11 @@
 
 function primeFactors($n, $primeFactor = 2)
 {
-    if ($n > $primeFactor && $n % $primeFactor == 0) {
-        return array_merge(array($primeFactor), primeFactors($n/ $primeFactor, $primeFactor));
-    } else {
-        $primeFactor++;
-        if ($n > $primeFactor && $n % $primeFactor == 0) {
-            return array_merge(array($primeFactor), primeFactors($n / $primeFactor, $primeFactor));
-        }
+    if($n <= $primeFactor) {
         return array($n);
     }
+    if($n % $primeFactor == 0) {
+        return array_merge(array($primeFactor), primeFactors($n / $primeFactor, $primeFactor));
+    }
+    return primeFactors($n, $primeFactor +1);
 }
